130339f6591efb2b589bce8c67f015a384e48d9a
Mohoshena <marin2003hstu@gmail.com>
2025-05-07 22:55:54 +0600

    Basics Day 4 part 2

---
+++ RDSML-Day-4 R progamming Basics (Part 2)/Basics day 4 part 2.R
@@ -0,0 +1,46 @@
+# Data Types in R
+# 1. Numeric - Integer / Floating Point
+# 2. String/Character - "Mohoshena"
+# 3. Logical
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = 'Mohoshena marine'
+print(name)
+class(name)
+c = "52"
+print(c + 9)
+print(a + 9)
+class(c)
+
+# Logical Data Type - TRUE, FALSE (T, F)
+class(a<b)
+print(a<b)
+x = 19
+y = 22-3
+print(x == y)
+print(x !=y)
+print(y > x)
+print(y >= x)
+
+# Logical Operators in R
+# && Logical Operator
+
+4 == 4 && 5 == 5
+
+x != y || x == y
+
+# Simple Mathematical Operators in R
+z = 6 + 9 - 8 * (2/3)
+print(z)
+
+w = 4 + 3 - 3 * 2/3
+print(w)
+w = (4 +3 - 3) * 2/3
+print(w)
+11/5
+11 %% 5
+
