6792e4d80648db4baa709a39991f12808896c582
Mohoshena <marin2003hstu@gmail.com>
2025-05-12 19:38:12 +0300

    Update Day-5 R as a vector.R

--- RDSML- Day-5 Vectors in R/Day-5 R as a vector.R
+++ RDSML- Day-5 Vectors in R/Day-5 R as a vector.R
@@ -24,3 +24,41 @@ is.numeric(a)
 a = as.character(a)
 a
 class(a)
+
+# logical operator TRUE and FALSE when converted
+# to numeric, TRUE converts to 1
+# and FALSE converts to 0
+d = c(TRUE, FALSE, FALSE, TRUE, FALSE)
+d = as.numeric(d)
+d
+
+# converting numeric to logical (1 or any number
+# other than 0 converts to TRUE, 0 converts to FALSE)
+d
+class(d)
+d = as.logical(d)
+d
+
+e = c(1, 0, 0, 1, 0, 23, -7, 0)
+e = as.logical(e)
+e
+
+# creating sequential vector
+# creating a vector starting from 1 to 10
+my_seq = c(1:100) 
+my_seq
+my_seq = c(1:50, 80, 99, 88,60, 77, 71, 77, 61, 92, 97)
+my_seq
+
+# creating a sequential vector using the seq() function
+# if seq is written, no need to use colon :
+# 
+new_seq = (seq(1,10))
+new_seq
+
+
+# sequence in steps of 3
+new_seq = c(seq(1, 10, by=3))
+new_seq
+
+# vectors can have character
