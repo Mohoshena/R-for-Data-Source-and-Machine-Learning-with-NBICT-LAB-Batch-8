90259b7597f961915c12e772998cc461001ae9ad
Mohoshena <marin2003hstu@gmail.com>
2025-05-12 19:12:15 +0300

    Day 5 R as a vector

---
+++ RDSML- Day-5 Vectors in R/Day-5 R as a vector.R
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+# when you have a text item in the vector
+b = c(5, 8, 2, "sv")
+b
+is.numeric(b)
+is.character(b)
+
+# we can convert data types
+# For example from numeric to character
+a = c(1, 2, 3, 4, 5)
+a
+class(a)
+is.numeric(a)
+a = as.character(a)
+a
+class(a)
--- RDSML-Day-3 R progamming Basics (Part 1)/r_basics_part1.R
+++ RDSML-Day-3 R progamming Basics (Part 1)/r_basics_part1.R
@@ -1,8 +1,14 @@
+#calculating_some_health_parameter
+
 my_age = 97
 my_weight = 60
 my_height = 65
 
 x = (my_height*my_age)/my_weight
 print(x)
-students_height = C(50, 55, 64, 71, 45.25)
+
+students_height = c(50, 55, 64, 71, 45.25)
 Mean(students_height)
+
+students_height = c(50, 55, 64, 71, 45.25)
+mean(students_height)
