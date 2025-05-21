cce6223a2e46b4adc748e3db777824098a7ee0c8
Mohoshena <marin2003hstu@gmail.com>
2025-05-21 19:22:28 +0300

    Day 8

---
+++ RDSML-Day-8 Descriptive statistics using the psych package/Day 8 Descriptive statistics using the psych package.R
@@ -0,0 +1,33 @@
+# Descriptive statistics using the psych package
+
+install.packages("psych")
+
+# need to load the package in environment 
+# to activate the package library By library function without inverted comma in bracket
+
+library(psych)
+
+# so many data sets are already stored in data set
+# we can use to practice any R program
+
+data()
+AirPassengers
+BOD
+
+# using the New York airport flight data
+install.packages("nycflights13")
+attach(nycflights13::flights)
+
+
+# Lets look at the first 6 records using the head() function
+head(nycflights13::flights)
+
+# psych package provides many functions and 
+# we saw the functions using library function
+# Using data set we can use during practice various functions in R
+# we attach the data sets and use in R
+
+# head() shows a part of the data set from the top in any range
+
+# 
+# 
