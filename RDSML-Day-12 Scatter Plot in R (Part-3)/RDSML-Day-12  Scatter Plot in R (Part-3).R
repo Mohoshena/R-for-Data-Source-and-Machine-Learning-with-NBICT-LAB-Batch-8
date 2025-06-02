73c0110b25ea543c4835c00d90c2fa6fe5b7d465
Mohoshena <marin2003hstu@gmail.com>
2025-06-02 20:26:45 +0300

    Update RDSML-Day-12  Scatter Plot in R (Part-3).R

--- RDSML-Day-12 Scatter Plot in R (Part-3)/RDSML-Day-12  Scatter Plot in R (Part-3).R
+++ RDSML-Day-12 Scatter Plot in R (Part-3)/RDSML-Day-12  Scatter Plot in R (Part-3).R
@@ -35,3 +35,66 @@ geom_point()+
   theme(legend.position = "top")
 
 # Changing point colors manually
+ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey'.'orange'))
+# Changing point colors manually
+ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey'.'orange'))+
+  # Changing point colors manually
+  ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +Add commentMore actions
+geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey','orange'))+
+  theme(legend.position = "top")
+
+# Customizing scatter plots
+ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey','orange'))+
+  labs(title="Miles per gallon according to the weight")+
+  theme(legend.position = "top")
+# Customizing scatter plots
+ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey','orange'))+
+  labs(title="Miles per gallon \ according to the weight")+
+  theme(legend.position = "top")
+# Customizing scatter plots
+ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey','orange'))+
+  labs(title="Miles per gallon \naccording to the weight")+
+  theme(legend.position = "top")
+# Customizing scatter plots
+ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey','orange'))+
+  labs(title="Miles per gallon \naccording to the weight",
+       x="weight", y="miles/gallon")+
+  theme(legend.position = "top")
+# Customizing themes
+plot = ggplot(dataset, aes(x = wt, y =mpg, color = cyl, shape = cyl)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE, fullrange = TRUE) +
+  scale_shape_manual(values=c(3,16,17)) +
+  scale_color_manual(values=c('darkblue', 'grey','orange'))+
+  labs(title="Miles per gallon \naccording to the weight",
+       x="weight", y="miles/gallon")+Add commentMore actions
+theme(legend.position = "top")
+plot + theme_classic()
+plot + theme_minimal()
