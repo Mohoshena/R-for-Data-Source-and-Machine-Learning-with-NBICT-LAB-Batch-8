c61d13895f76be76ff6b0d31fa4958f354af3e0e
Mohoshena <marin2003hstu@gmail.com>
2025-06-02 20:21:16 +0300

    D:\R NBICT\R-for-Data-Source-and-Machine-Learning-with-NBICT-LAB-Batch-8\RDSML-Day-12 Scatter Plot in R (Part-3)\RDSML-Day-12  Scatter Plot in R (Part-3)

---
+++ RDSML-Day-12 Scatter Plot in R (Part-3)/RDSML-Day-12  Scatter Plot in R (Part-3).R
@@ -0,0 +1,37 @@
+library(ggplot2)
+dataset=mtcars
+
+# Converting cyl column froma numeric to a factor variable
+dataset$cyl = as.factor(dataset$cyl)
+
+# Adding regression lines based on multiple groups
+ggplot(dataset, aes(x=wt, y= mpg, color=cyl))+
+  geom_point()+
+geom_smooth(method= lm)  
+geom_smooth(method= lm, se=FALSE) 
+
+# Removing the regression lines based on multiple groups
+ggplot(dataset, aes(x=wt, y= mpg, color=cyl, shape=cyl))+
+  geom_point()+
+  geom_smooth(method= lm)  
+geom_smooth(method= lm, se=FALSE) 
+
+# Extending the regression lines
+ggplot(dataset, aes(x=wt, y= mpg, color=cyl, shape=cyl))+
+  geom_point()+
+geom_smooth(method= lm, se=FALSE, fullrange=TRUE) 
+
+# Filling the color of confidence bands
+ggplot(dataset, aes(x=wt, y= mpg, color=cyl, shape=cyl))+
+  geom_point()+
+geom_smooth(method= lm, ase=fullrange) 
+
+# Changing point shapes manually
+
+ggplot(dataset, aes(x=wt, y=mpg, color=cyl, shape=cyl))+
+geom_point()+
+  geom_smooth(method=lm, se=FALSE, fullrange=TRUE)+
+  scale_shape_manual(values=c(3,16,17))+
+  theme(legend.position = "top")
+
+# Changing point colors manually
